f88fab34b6bb2f06d7a050a45666d5d23ec60ee2
adcad022 <averchot@chicagobooth.edu>
2015-07-22 10:31:50 -0500

    Final

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -10,6 +10,18 @@ class MeteorologistController < ApplicationController
     @street_address = params[:user_street_address]
     url_safe_street_address = URI.encode(@street_address)
 
+    urlm = "http://maps.googleapis.com/maps/api/geocode/json?address=#{url_safe_street_address}"
+    
+    parsed_datam = JSON.parse(open(urlm).read)
+
+    latm = parsed_datam["results"][0]["geometry"]["location"]["lat"]
+
+    lngm = parsed_datam["results"][0]["geometry"]["location"]["lng"] 
+
+    urlf = "https://api.forecast.io/forecast/0aa57b84821ba837319e7139219885f2/#{latm},#{lngm}"
+
+    parsed_dataf = JSON.parse(open(urlf).read)
+
     # ==========================================================================
     # Your code goes below.
     # The street address the user input is in the string @street_address.
@@ -17,17 +29,15 @@ class MeteorologistController < ApplicationController
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
 
+    @current_temperature = parsed_dataf["currently"]["temperature"]
 
+    @current_summary = parsed_dataf["currently"]["summary"]
 
-    @current_temperature = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = parsed_dataf["minutely"]["summary"]
 
-    @current_summary = "Replace this string with your answer."
+    @summary_of_next_several_hours = parsed_dataf["hourly"]["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
-
-    @summary_of_next_several_hours = "Replace this string with your answer."
-
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = parsed_dataf["daily"]["summary"]
 
     render("street_to_weather.html.erb")
   end
